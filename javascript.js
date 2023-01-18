4c4f951282f60ea625f9e15ae3e2fed27e699e19
Grace <99232016+gracefry@users.noreply.github.com>
2023-01-19 02:10:03 +1100

    Add pop-up form, adding to array working
    Form hides initially and with the "close" button, opens with the "New book"
    button.
    Form inputs are constructed into a new Book object, and are added to the
    myLibrary array successfully.
    
    Current problems:
    Array in DOM is repeating, not refreshing
    Ugly

--- javascript.js
+++ javascript.js
@@ -15,8 +15,14 @@ function addBookToLibrary(book) {
 
 function displayBooks() {
     for (book of myLibrary) {
+        let read = "unread";
+
+        if (book.hasRead) {
+            read = "read";
+        }
+
         let newBook = document.createElement("p");
-        newBook.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.hasRead}.`;
+        newBook.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${read}.`;
         shelf.appendChild(newBook);
     }
 }
@@ -24,4 +30,33 @@ function displayBooks() {
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 addBookToLibrary(theHobbit);
 displayBooks();
+
+const form = document.querySelector(".form-popup");
+const openButton = document.getElementById("open-form");
+const closeButton = document.getElementById("close-form");
+const addButton = document.getElementById("add-book");
+
+
+function openForm() {
+    form.style.display = "flex";
+}
+
+function closeForm() {
+    form.style.display = "none";
+}
+
+openButton.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
+addButton.addEventListener("click", () => {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+
+    let book = new Book(author, title, pages, read);
+    addBookToLibrary(book);
+    displayBooks();
+});
+
+
 
